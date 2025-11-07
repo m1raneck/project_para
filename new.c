5194c240c5f7a3bb8006a1fc6f81e8c644346f3c
Evgenii <moreoline@gmail.com>
2025-11-07 11:19:16 +1000

    second_commit

--- new.c
+++ new.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+#include <math.h>
+
+int pow(int a, int b) { return pow(a, b); }
 
 int main( void )
 {
     printf("hello world\n");
+    printf("%d", pow(2, 3));
     return 0;
 }
